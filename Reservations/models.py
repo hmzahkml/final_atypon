744b2347d3b7962f130c3a44ea9ed267dbf81359
hmzahkml <kmalhmzh99@gmail.com>
2023-03-06 23:18:57 +0300

    working models

--- Reservations/models.py
+++ Reservations/models.py
@@ -1,3 +1,17 @@
+#rooms model
 from django.db import models
 
+from Rooms.models import Room
+
 # Create your models here.
+
+class reservation(models.Model):
+
+
+    reserved_at = models.DateTimeField(auto_now_add=True)
+    start_date = models.DateTimeField()
+    offer = models.ForeignKey(Room, on_delete=models.CASCADE,related_name='room')
+    email = models.EmailField(blank=False)
+    phone_number = models.IntegerField(blank=True)
+    number_days= models.IntegerField(blank=False)
+    is_canceled= models.BooleanField(default=False)
